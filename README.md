ce1602b4024d911d1a4abf1fe74a41b8a04b1582
quim <quim@MacBook-Pro-de-quim-2.local>
2016-03-31 19:51:34 +0100

    add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# pad
+
+
+```bash
+
+```
+
+```js
+var pad = require('pad')
+
+console.log(pad.left('foo', 5)) // print `  foo`
+console.log(pad.right('bar', 10, '.')) // prints `bar.......`
+
+```
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict'
 
-var pad = module.exports = {left: left, right: right}
+module.exports = {left: left, right: right}
 
 function left (str, len, ch) {
   return repeat(str, len, ch) + String(str)
 }
 
 function right (str, len, ch) {
- return String(str) + repeat(str, len, ch)
+  return String(str) + repeat(str, len, ch)
 }
 
 function repeat (str, len, ch) {
